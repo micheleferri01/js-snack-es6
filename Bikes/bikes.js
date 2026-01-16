00c1ad1a7be004987f207d2c631e4355aa769acd
micheleferri01 <micheleferri095@gmail.com>
2026-01-16 14:31:57 +0100

    Creato array di oggetti per le bici

---
+++ Bikes/bikes.js
@@ -0,0 +1,16 @@
+// # Richiesta
+//* Creare un array di oggetti:
+//* Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//* Stampare a schermo la bici con peso minore.
+
+
+// array di oggetti contenenti i dati delle bici
+const bikes = [
+    {nome: "Race", peso: 10,},
+    {nome: "Endurance", peso: 15},
+    {nome: "Aero", peso: 13},
+    {nome: "Scott Addict RC", peso: 6},
+    {nome: "Giant TCR Advanced SL", peso: 8}
+];
+
+console.table(bikes);
